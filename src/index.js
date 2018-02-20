4056f65b6a1d746c3e9e22894a6e4af478750777
Vignesh <vrs23.92@gmail.com>
2018-02-20 12:39:51 +0530

    Added transitions
    Added css transitions and updated sound logic.

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   if(!audio) return;
 
+  resetTransition();
+
   audio.currentTime = 0;
   audio.play();
+  document.querySelector(`div[data-key="${e.keyCode}"]`).classList.add("playing");
   console.log(audio);
 }
 
-const keys = document.querySelectorAll(".keys");
-  console.log(keys);
-  keys.forEach(function(key) {
-    console.log(key);
-    //key.
+function resetTransition() {
+  const keys = document.querySelectorAll(".key");
+  keys.forEach((key) => {
+    key.classList.remove("playing");
   });
+}
+
+  
 window.addEventListener('keydown', playSound);
